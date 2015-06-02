15dfebb0f482a65e4eca548c38e360d911397c8d
Vadim Senderovich <vadims@commpeak.com>
2015-06-02 19:24:09 +0300

    added support for locales and rtl support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def available_locales
+    {
+      en: 'English',
+      he: 'עברית',
+      ru: 'Русский'
+    }
+  end
+
+  def right_left
+    I18n.locale == :he ? 'left' : 'right'
+  end
 end
